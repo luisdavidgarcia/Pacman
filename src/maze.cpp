6f72738762f3e91c848ceb3477850818c37be4da
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-04-28 00:36:19 -0700

    Updated the maze with proper constructor

--- src/maze.cpp
+++ src/maze.cpp
@@ -2,45 +2,189 @@
 
 Maze::Maze() {
   grid = {{
-    {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1},
-    {1,0,0,0, 0,0,0,0, 0,0,1,0, 0,0,0,0, 0,0,0,0, 1},
-    {1,0,1,1, 1,0,1,1, 1,0,1,0, 1,1,1,0, 1,1,1,0, 1},
-    {1,0,1,1, 1,0,1,1, 1,0,1,0, 1,1,1,0, 1,1,1,0, 1},
-    {1,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 1},
-    {1,0,1,1, 1,0,1,0, 1,1,1,1, 1,0,1,0, 1,1,1,0, 1},
-    {1,0,0,0, 0,0,1,0, 0,0,1,0, 0,0,1,0, 0,0,0,0, 1},
-    {1,1,1,1, 1,0,1,1, 1,0,1,0, 1,1,1,0, 1,1,1,1, 1},
-    {0,0,0,0, 1,0,1,0, 0,0,0,0, 0,0,1,0, 1,0,0,0, 0},
-    {1,1,1,1, 1,0,1,0, 1,1,0,1, 1,0,1,0, 1,1,1,1, 1},
-    {0,0,0,0, 0,0,0,0, 1,0,0,0, 1,0,0,0, 0,0,0,0, 1},
-    {1,1,1,1, 1,0,1,0, 1,0,0,0, 1,0,1,0, 1,1,1,1, 1},
-    {0,0,0,0, 1,0,1,0, 1,1,1,1, 1,0,1,0, 1,0,0,0, 0},
-    {0,0,0,0, 1,0,1,0, 0,0,0,0, 0,0,1,0, 1,0,0,0, 0},
-    {1,1,1,1, 1,0,0,0, 1,1,1,1, 1,0,0,0, 1,1,1,1, 1},
-    {1,0,0,0, 0,0,0,0, 0,0,1,0, 0,0,0,0, 0,0,0,0, 1},
-    {1,0,1,1, 1,0,1,1, 1,0,1,0, 1,1,1,0, 1,1,1,0, 1},
-    {1,0,0,0, 1,0,0,0, 0,0,0,0, 0,0,0,0, 1,0,0,0, 1},
-    {1,1,0,0, 1,0,1,0, 1,1,1,1, 1,0,1,0, 1,0,0,1, 1},
-    {1,0,0,0, 0,0,1,0, 0,0,1,0, 0,0,1,0, 0,0,0,0, 1},
-    {1,0,1,1, 1,1,1,1, 1,0,1,0, 1,1,1,1, 1,1,1,0, 1},
-    {1,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 1},
-    {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1},
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall },
+    { MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty,MazeCell::Empty,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty },
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Empty,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall },
+    { MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty,MazeCell::Pellet,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Empty,MazeCell::Pellet, 
+      MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty },
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall },
+    { MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty },
+    { MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Empty,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty,MazeCell::Empty,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Empty,MazeCell::Empty, 
+      MazeCell::Empty },
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Empty, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Empty,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Empty,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Pellet,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet,MazeCell::Pellet, 
+      MazeCell::Wall },
+    { MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall,MazeCell::Wall,MazeCell::Wall,MazeCell::Wall, 
+      MazeCell::Wall },
   }};
 }
 
-void Maze::draw(int tileSize) const {
+void Maze::draw(int tileSize) const 
+{
+  float radius = tileSize / 10.0f;
+  int tileCenter = tileSize / 2;
+
   for (int row = 0; row < ROWS; ++row) {
     for (int col = 0; col < COLS; ++col) {
-      if (grid[row][col] == 1) {
-        DrawRectangle(
-            col * tileSize,
-            row * tileSize,
-            tileSize,
-            tileSize,
-            BLUE
+      float x = col * tileSize;
+      float y = row * tileSize;
+
+      if (grid[row][col] == MazeCell::Wall) {
+        DrawRectangle(x, y, tileSize, tileSize, BLUE);
+      } else if (grid[row][col] == MazeCell::Pellet) {
+        DrawCircle(
+            x + tileCenter, 
+            y + tileCenter, 
+            radius, 
+            YELLOW
         );
       }
     }
   }
+}
+
+MazeCell Maze::getTile(int row, int col) 
+{
+  if (row < 0 || row > ROWS-1) 
+    std::cerr << "Improper row input" << std::endl;
+  else if (col < 0 || col > COLS-1)
+    std::cerr << "Improper column input" << std::endl;
+
+  return grid[row][col];
+}
+
+void Maze::collectPellet(int row, int col)
+{
+  if (row < 0 || row > ROWS-1) 
+    std::cerr << "Improper row input" << std::endl;
+  else if (col < 0 || col > COLS-1)
+    std::cerr << "Improper column input" << std::endl;
+
+  if (grid[row][col] == MazeCell::Pellet)
+    grid[row][col] = MazeCell::Empty;
 }
 
