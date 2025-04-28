97ed9e4fb89a44a99f5c357698c77ff228435034
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-04-28 00:33:31 -0700

    Fixed maze to proper ones

--- include/maze.hpp
+++ include/maze.hpp
@@ -4,17 +4,30 @@
 
 #include "raylib.h"
 #include <array>
+#include <iostream>
+
+enum class MazeCell
+{
+  Empty,
+  Wall,
+  Pellet,
+  PowerPellet
+};
 
 class Maze 
 {
-private:
+public:
   static constexpr int ROWS = 23;
   static constexpr int COLS = 21;
-  std::array<std::array<int, COLS>, ROWS> grid;
+  static constexpr int TILE_SIZE = 32;
 
-public:
   Maze();
-  void draw(int tileSize = 32) const;
+  void draw(int tileSize = TILE_SIZE) const;
+  MazeCell getTile(int row, int col);
+  void collectPellet(int row, int col);
+
+private:
+  std::array<std::array<MazeCell, COLS>, ROWS> grid;
 };
 
 #endif // MAZE_HPP
