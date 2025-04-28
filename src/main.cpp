2249dc35db02793e6b6ea6cf6c2941c34d304fbb
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-04-28 00:35:47 -0700

    Updated to include pacman and some game logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "raylib.h"
 #include "maze.hpp"
+#include "pacman.hpp"
 #include <cstdlib>
 
 int main(void) 
 {
+  Pacman pacman;
   Maze maze;
 
   InitWindow(700, 750, "Pacman!");
@@ -13,11 +15,22 @@ int main(void)
     BeginDrawing();
     ClearBackground(BLACK);
 
-    //DrawText("Hello Pac-Man!", 300, 280, 20, YELLOW);
-    maze.draw();
-
+    if (pacman.gameOver) {
+      DrawText("GAME OVER", 250, 350, 40, RED);
+      DrawText("Press R to Restart", 250, 400, 20, WHITE);
+    } else {
+      pacman.update(maze);
+      pacman.draw();
+      maze.draw();
+    }
 
     EndDrawing();
+
+    if (pacman.gameOver && IsKeyPressed(KEY_R)) {
+      pacman = Pacman(10, 17, 5);
+      pacman.gameOver = false;
+      maze = Maze();
+    }
   }
 
   CloseWindow();
